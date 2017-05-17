e9ad3b07d0181b8c72c0d4b7a0b8fd637f7fca80
Susmitha Begari <sbegari@evoketechnologies.com>
2017-05-17 20:43:32 +0530

    fixed 4960 and few chnages

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -2,35 +2,46 @@ import {Component, OnInit, OnChanges} from '@angular/core';
 import {Router} from '@angular/router';
 import {LangEnglishService} from "../shared/english";
 import {AppComponent} from "../app.component";
+import {ToastrService} from 'toastr-ng2';
+import {AppUtilities} from "../shared/appUtilities";
 import {CollectionService} from "../shared/data.service";
 import {CollectionData} from "../collection/collection-data";
 
-import {ToastrService} from 'toastr-ng2';
-declare var $:any;
+declare var $: any;
 @Component({
     selector: 'app-banner',
     templateUrl: './banner.component.html',
     styleUrls: ['./banner.component.less']
 })
 export class BannerComponent implements OnInit {
-    zip:any;
+    zip: any;
+    zipCode: any;
+    lang: any;
 
-    zipCode:any;
-    lang:any;
 
-    constructor(private appComponent:AppComponent
-        , private route:Router
-        , private localize:LangEnglishService
-        , private dataService:CollectionService
-        , private data:CollectionData) {
+    constructor(private appComponent: AppComponent
+        , private route: Router
+        , private toastr: ToastrService
+        , private localize: LangEnglishService
+        , private dataService: CollectionService
+        , private dataStore: CollectionData, ) {
     }
-
     save(form, event) {
         event.preventDefault();
         $('body').addClass('loader');
-        this.route.navigate(['/zipResults', form.value.zip])
-    }
+        this.dataService.getZipResults(form.value.zip)
+            .subscribe(
+            res => {
+                this.dataStore.zipResults = res;
+                $('body').removeClass('loader');
+                this.route.navigate(['/zipResults', form.value.zip])
+            },
+            error => {
+                this.toastr.error(`${form.value.zip} is not correct, try with another one`);
+                $('body').removeClass('loader');
+            });
 
+    }
     ngOnChanges() {
         console.log('changed');
     }
