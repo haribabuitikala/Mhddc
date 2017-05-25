0f555dd0d98da740bac6f62fe68214132734097b
jagadeespenta <jagadeeshpc18@gmail.com>
2017-05-25 17:20:23 +0530

    4971-fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,28 +1,28 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {AppComponent} from "../app.component";
-import {Router} from '@angular/router';
-import {AppUtilities} from "../shared/appUtilities";
+import { Component, OnInit, Input } from '@angular/core';
+import { AppComponent } from "../app.component";
+import { Router } from '@angular/router';
+import { AppUtilities } from "../shared/appUtilities";
 
-declare var $:any;
+declare var $: any;
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.less'],
     host: {
-        '(document:click)': 'humbergerCollapse($event)',
+        '(document:click)': 'humbergerCollapse()',
     }
 })
 export class HeaderComponent implements OnInit {
-    showhamburger:boolean = false;
+    showhamburger: boolean = false;
 
-    @Input() count:any;
-    @Input() showHamburger:boolean = false;
+    @Input() count: any;
+    @Input() showHamburger: boolean = false;
     itemsCount = this.utils.utilities.itemsCount;
 
-    constructor(private appComponent:AppComponent
-        , private route:Router
-        , private utils:AppUtilities) {
+    constructor(private appComponent: AppComponent
+        , private route: Router
+        , private utils: AppUtilities) {
     }
 
     homePage(path) {
@@ -37,31 +37,26 @@ export class HeaderComponent implements OnInit {
         this.humbergerCollapse();
     }
 
-    humberger(event?: MouseEvent) {
-        if(event){
-            if(!$(event.target).hasClass('menu-collapse')){
-                $('.nav-component').animate({width: '185px'}, function () {
-                    $('.nav-component li span').removeClass('hide');
-                    $('.collapse-humberger').removeClass('hide');
-                    $('.hamburger').addClass('hide');
-                    $('body').addClass('menu-open');
-                });
-            }
+    humberger() {
+        if (!$('.menu-collapse').is(":visible")) {
+            $('.nav-component').animate({ width: '185px' }, function () {
+                $('.nav-component li span').removeClass('hide');
+                $('.collapse-humberger').removeClass('hide');
+                $('.hamburger').addClass('hide');
+                $('body').addClass('menu-open');
+            });
         }
-        
     }
 
-    humbergerCollapse(event?: MouseEvent) {
-        if(event) {
-            if($(event.target).hasClass('menu-collapse') || $(event.target).hasClass('menu-open')) {
-                $('.nav-component li span').addClass('hide');
-                $('.nav-component').animate({width: '16.66666667%'}, function () {
-                    $('.collapse-humberger').addClass('hide');
-                    $('.hamburger').removeClass('hide');
-                    $('body').removeClass('menu-open');
-                });
-            }  
-        }              
+    humbergerCollapse() {
+        if ($('.menu-collapse').is(":visible")) {
+            $('.nav-component li span').addClass('hide');
+            $('.nav-component').animate({ width: '16.66666667%' }, function () {
+                $('.collapse-humberger').addClass('hide');
+                $('.hamburger').removeClass('hide');
+                $('body').removeClass('menu-open');
+            });
+        }
     }
 
 }
