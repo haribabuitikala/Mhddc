05d02378cf8bf085965867d51eb19d8951043643
sbegari <sbegari@evoketechnologies.com>
2017-06-04 05:21:26 +0530

    added customer forms validations and few issues

--- src/app/customer-info/customer-info.component.ts
+++ src/app/customer-info/customer-info.component.ts
@@ -7,18 +7,61 @@ import {AppUtilities} from "../shared/appUtilities";
 import {CollectionService} from "../shared/data.service";
 import {CollectionData} from "../collection/collection-data";
 
+
 @Component({
+    moduleId: module.id,
     selector: 'app-customer-info',
     templateUrl: './customer-info.component.html',
     styleUrls: ['./customer-info.component.less']
 })
+
 export class CustomerInfoComponent implements OnInit {
 
+
+    user;
     constructor(private route: Router) {}
 
+
     ngOnInit() {
+        this.user = {
+            txtFirstName_SoldTo: '',
+            txtLastName_SoldTo: '',
+            txtAddress1_SoldTo: '',
+            txtAddress2_SoldTo: '',
+            txtCity_SoldTo: '',
+            txtState_SoldTo: '',
+            txtZipCode_SoldTo: '',
+            txtCounty_SoldTo: '',
+            txtPhoneNumber_SoldTo: '',
+            txtSecondaryNumber_SoldTo: '',
+            txtCustomerEmail_SoldTo: ''
+        };
     }
-    goToCustomerServiceInfo() {
-        this.route.navigateByUrl('/customer-service-info');
+
+    save(values, valid) {
+
+        console.log(values, valid)
+        if (valid) {
+            this.route.navigateByUrl('/customer-service-info');
+        } else {
+
+        }
     }
+
+//    goToCustomerServiceInfo() {
+//        this.route.navigateByUrl('/customer-service-info');
+//    }
+    onlyNumberKey(e) {
+        var valid = (e.which >= 48 && e.which <= 57) || (e.which >= 65 && e.which <= 90) || (e.which >= 97 && e.which <= 122);
+        if (!valid) {
+            e.preventDefault();
+        }
+    }
+    disAllowNumbers(e) {
+        var valid = (e.which >= 65 && e.which <= 90) || (e.which >= 97 && e.which <= 122);
+        if (!valid) {
+            e.preventDefault();
+        }
+    }
+
 }
