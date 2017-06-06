36467bad8413c3a0754443c4a1e0ebe2d285a372
Venkata Satya Sai Kiran Mantha <vmantha@evoketechnologies.com>
2017-06-06 11:05:09 +0530

    moved loader to callback function

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -107,6 +107,7 @@ export class ConfigComponent implements OnInit, AfterViewInit {
             VIEW: 'door',
             doneCallback: () => {
                 this.generateDoorWithHome().then(({ canvas }) => {
+                    this.utils.setLoader();
                     $('#homeVis').html('').append(canvas);
                     $('#homeVis').find('canvas').css({
                         height: '100%',
@@ -149,220 +150,216 @@ export class ConfigComponent implements OnInit, AfterViewInit {
 
     renderCanvas(obj?, targ?, elemSelector?) {
         this.utils.setLoader();
-        this.getVisUpdate(obj, targ, elemSelector).then(res => {
-            this.utils.removeLoader();
-        });        
+        this.getVisUpdate(obj, targ, elemSelector);
     }
 
     getVisUpdate(obj?, targ?, elemSelector?) {
-        return new Promise((response) => {
-            if (typeof (targ) === 'undefined') targ = 'doorVis';
-            if (typeof (obj) === 'undefined') obj = this.utils.resFlowSession.resDoorObj;
+        if (typeof (targ) === 'undefined') targ = 'doorVis';
+        if (typeof (obj) === 'undefined') obj = this.utils.resFlowSession.resDoorObj;
 
-            var viewD = 'door'
+        var viewD = 'door'
 
-            if (targ == 'doorVis') {
-                targ = $('#doorVis');
-            } else {
-                viewD = 'home'
-                targ = $('#homeVis');
+        if (targ == 'doorVis') {
+            targ = $('#doorVis');
+        } else {
+            viewD = 'home'
+            targ = $('#homeVis');
+        }
+
+        if (elemSelector) {
+            targ = $(elemSelector);
+        }
+
+
+        var buildObj = {
+            centergrooves: 0,
+            claddingswap: "",
+            colorcode: "",
+            colorswaprule: "",
+            constructionmodel: "",
+            constructionswaprule: "",
+            designimage: "",
+            doorcolumns: 0,
+            doorrows: 0,
+            glaz: "",
+            handleplacement: "",
+            hardwarehandle: "",
+            hardwarehinge: "",
+            hardwarestepplate: "",
+            hingeplacement: "",
+            overlaycolor: "",
+            productid: 0,
+            stepplateplacement: "",
+            topsectionimage: "",
+            topsectionrow: "0",
+            callBack: () => {
+                this.utils.removeLoader();
+                console.log('rendering done!!!!!!');
             }
+        };
 
-            if (elemSelector) {
-                targ = $(elemSelector);
-            }
+        var dor = obj
 
-
-            var buildObj = {
-                centergrooves: 0,
-                claddingswap: "",
-                colorcode: "",
-                colorswaprule: "",
-                constructionmodel: "",
-                constructionswaprule: "",
-                designimage: "",
-                doorcolumns: 0,
-                doorrows: 0,
-                glaz: "",
-                handleplacement: "",
-                hardwarehandle: "",
-                hardwarehinge: "",
-                hardwarestepplate: "",
-                hingeplacement: "",
-                overlaycolor: "",
-                productid: 0,
-                stepplateplacement: "",
-                topsectionimage: "",
-                topsectionrow: "0",
-                callBack: () => {
-                    console.log('rendering done!!!!!!');
-                }
-            };
-
-            var dor = obj
-
-            if (dor.TYPE != "GDO") {
-                if (dor.product.product && dor.product.product != '') buildObj.productid = Number(dor.product.product.item_id);
-                if (dor.design != '') {
-                    buildObj.designimage = dor.design.dsgn.visimage;
-                    buildObj.doorcolumns = Number(dor.design.columns);
-                    buildObj.doorrows = Number(dor.design.rows);
-
-                    if (this.navComponent.flowType === 'resquick') {
-                        buildObj.doorcolumns = Number(dor.design.dsgn.Columns);
-                        buildObj.doorrows = Number(dor.design.dsgn.Rows);
-                    }
-                }
-
-
-                if (dor.construction.construction != '') {
-                    buildObj.constructionswaprule = dor.construction.construction.constructionswaprule;
-                }
-
-                if (dor.construction.cladding != '') {
-                    buildObj.constructionswaprule = dor.construction.cladding.imageswaprule;
-                }
-                try {
-                    if (dor.construction.groove != '') {
-                        buildObj.centergrooves = dor.construction.groove.nogrooves;
-                    }
-                } catch (e) { }
-
-                if (dor.color.base && dor.color.base != '' && dor.color.base['colorcode']) {
-                    buildObj.colorcode = dor.color.base.colorcode;
-                    buildObj.overlaycolor = ".94,.94,.94,1,25,25,25,0";
-
-                }
-                if (dor.color.base && dor.color.base != '' && dor.color.base['colorcode']) {
-                    buildObj.colorswaprule = dor.color.base.colorswaprule;
-                }
-
-                if (dor.product.product.item_id == 11) {
-                    buildObj.overlaycolor = dor.color.base.colorcode;
-                    buildObj.colorcode = ".94,.94,.94,1,25,25,25,0";
-                    if (dor.color.overlay != '') {
-                        buildObj.colorcode = dor.color.overlay.colorcode;
-                    }
-
-
-                }
-
-
-                if (dor.product.product.item_id == 9) {
-                    buildObj.overlaycolor = dor.color.base.colorcode;
-                    buildObj.colorcode = dor.color.base.colorcode;
-                    if (dor.color.overlay != '') {
-                        buildObj.colorcode = dor.color.base.colorcode;
-                    }
-                }
-
-
-                if (Number(dor.product.product.item_id) == 30) {
-                    buildObj.overlaycolor = '';
-                }
-
-                if (dor.windows.topsection != '') {
-                    buildObj.topsectionimage = dor.windows.topsection.visimage;
-                    try {
-                        if (dor.windows.glasstype.Config == undefined) {
-                            if (dor.windows.topsection.glasstypes == undefined) {
-                                buildObj['glaz'] = 'GLAZ-SOL'
-                            }
-                            else {
-                                buildObj.glaz = dor.windows.topsection.glasstypes[0].Config;
-                            }
-                        }
-                        else {
-                            buildObj.glaz = dor.windows.glasstype.Config;
-                        }
-                    }
-                    catch (e) { }
-                }
+        if (dor.TYPE != "GDO") {
+            if (dor.product.product && dor.product.product != '') buildObj.productid = Number(dor.product.product.item_id);
+            if (dor.design != '') {
+                buildObj.designimage = dor.design.dsgn.visimage;
+                buildObj.doorcolumns = Number(dor.design.columns);
+                buildObj.doorrows = Number(dor.design.rows);
 
                 if (this.navComponent.flowType === 'resquick') {
-                    buildObj.topsectionimage = dor.construction.construction.Topsections[0].visimage;
-                    try {
-                        if (dor.windows.glasstype.Config == undefined) {
-                            if (dor.construction.construction.Topsections[0].glasstypes == undefined) {
-                                buildObj['glaz'] = dor.construction.construction.Topsections[0].Config;
-                            }
-                            else {
-                                buildObj.glaz = dor.construction.construction.Topsections[0].glasstypes[0].Config;
-                            }
+                    buildObj.doorcolumns = Number(dor.design.dsgn.Columns);
+                    buildObj.doorrows = Number(dor.design.dsgn.Rows);
+                }
+            }
+
+
+            if (dor.construction.construction != '') {
+                buildObj.constructionswaprule = dor.construction.construction.constructionswaprule;
+            }
+
+            if (dor.construction.cladding != '') {
+                buildObj.constructionswaprule = dor.construction.cladding.imageswaprule;
+            }
+            try {
+                if (dor.construction.groove != '') {
+                    buildObj.centergrooves = dor.construction.groove.nogrooves;
+                }
+            } catch (e) { }
+
+            if (dor.color.base && dor.color.base != '' && dor.color.base['colorcode']) {
+                buildObj.colorcode = dor.color.base.colorcode;
+                buildObj.overlaycolor = ".94,.94,.94,1,25,25,25,0";
+
+            }
+            if (dor.color.base && dor.color.base != '' && dor.color.base['colorcode']) {
+                buildObj.colorswaprule = dor.color.base.colorswaprule;
+            }
+
+            if (dor.product.product.item_id == 11) {
+                buildObj.overlaycolor = dor.color.base.colorcode;
+                buildObj.colorcode = ".94,.94,.94,1,25,25,25,0";
+                if (dor.color.overlay != '') {
+                    buildObj.colorcode = dor.color.overlay.colorcode;
+                }
+
+
+            }
+
+
+            if (dor.product.product.item_id == 9) {
+                buildObj.overlaycolor = dor.color.base.colorcode;
+                buildObj.colorcode = dor.color.base.colorcode;
+                if (dor.color.overlay != '') {
+                    buildObj.colorcode = dor.color.base.colorcode;
+                }
+            }
+
+
+            if (Number(dor.product.product.item_id) == 30) {
+                buildObj.overlaycolor = '';
+            }
+
+            if (dor.windows.topsection != '') {
+                buildObj.topsectionimage = dor.windows.topsection.visimage;
+                try {
+                    if (dor.windows.glasstype.Config == undefined) {
+                        if (dor.windows.topsection.glasstypes == undefined) {
+                            buildObj['glaz'] = 'GLAZ-SOL'
                         }
                         else {
-                            buildObj.glaz = dor.windows.glasstype.Config;
-                        }
-
-                    }
-                    catch (e) { }
-                }
-
-
-                buildObj['glassSection'] = dor.windows.selectedGlassSection;
-                //shankar added this, for restopsection placement
-                if (dor.windows.placement && dor.windows.placement != '') {
-                    buildObj.topsectionrow = dor.windows.placement.item_id;
-                }
-                //if (dor.windows.placement != '') buildObj.topsectionrow = dor.windows.placement;
-                if (dor.TYPE == "COM") {
-                    // shankar added this, for comtopsection placement
-                    if (dor.windows.placement != '') {
-                        buildObj.topsectionrow = dor.windows.placement;
-                        if (dor.windows.placement['item_id']) {
-                            buildObj.topsectionrow = dor.windows.placement['item_id'];
+                            buildObj.glaz = dor.windows.topsection.glasstypes[0].Config;
                         }
                     }
-                    if (dor.windows.glasstypePlacement != "") {
-                        var separators = ['th', 'rd', 'nd'];
-                        var placement = dor.windows.glasstypePlacement;
-                        var res = placement.split("-");
-                        res = res[1].split(new RegExp('[' + separators.join('') + ']', 'g'));
-                        //buildObj.topsectionrow = res[0]; //added shankar for comtopsection placement
+                    else {
+                        buildObj.glaz = dor.windows.glasstype.Config;
                     }
                 }
+                catch (e) { }
+            }
 
-
+            if (this.navComponent.flowType === 'resquick') {
+                buildObj.topsectionimage = dor.construction.construction.Topsections[0].visimage;
                 try {
-                    if (dor.hardware.hinge != '') {
-                        buildObj.hardwarehinge = dor.hardware.hinge.visimage;
-                        buildObj.hingeplacement = dor.hardware.hinge.placement;
-                        if (dor.hardware.hinge.count) {
-                            buildObj.hingeplacement = dor.hardware.hinge.placementlist.split(';')[dor.hardware.hinge.count - 1];
-                        } else {
-                            buildObj.hingeplacement = dor.hardware.hinge.placementlist.split(';')[0];
+                    if (dor.windows.glasstype.Config == undefined) {
+                        if (dor.construction.construction.Topsections[0].glasstypes == undefined) {
+                            buildObj['glaz'] = dor.construction.construction.Topsections[0].Config;
                         }
-
+                        else {
+                            buildObj.glaz = dor.construction.construction.Topsections[0].glasstypes[0].Config;
+                        }
+                    }
+                    else {
+                        buildObj.glaz = dor.windows.glasstype.Config;
                     }
 
-                } catch (e) { }
-
-
-                if (dor.hardware.stepplate != '') {
-                    buildObj.hardwarestepplate = dor.hardware.stepplate.visimage;
-                    buildObj.stepplateplacement = dor.hardware.stepplate.placement;
                 }
-                if (dor.hardware.handle != '') {
-                    buildObj.hardwarehandle = dor.hardware.handle.visimage;
-                    buildObj.handleplacement = dor.hardware.handle.placement;
-                }
-
-
-                if (buildObj['overlay'] != undefined) {
-                    buildObj['overlay'] = '';
-                }
-            }
-            else {
-                if (dor.opener.opener != '') {
-                    buildObj.productid = Number(dor.opener.opener.item_id);
-                    buildObj.designimage = dor.opener.opener.item_thumbnail;
-                }
+                catch (e) { }
             }
 
-            targ.DoorVisualization('view', viewD)
-            targ.DoorVisualization('update', buildObj)
-            response(true);
-        });
+
+            buildObj['glassSection'] = dor.windows.selectedGlassSection;
+            //shankar added this, for restopsection placement
+            if (dor.windows.placement && dor.windows.placement != '') {
+                buildObj.topsectionrow = dor.windows.placement.item_id;
+            }
+            //if (dor.windows.placement != '') buildObj.topsectionrow = dor.windows.placement;
+            if (dor.TYPE == "COM") {
+                // shankar added this, for comtopsection placement
+                if (dor.windows.placement != '') {
+                    buildObj.topsectionrow = dor.windows.placement;
+                    if (dor.windows.placement['item_id']) {
+                        buildObj.topsectionrow = dor.windows.placement['item_id'];
+                    }
+                }
+                if (dor.windows.glasstypePlacement != "") {
+                    var separators = ['th', 'rd', 'nd'];
+                    var placement = dor.windows.glasstypePlacement;
+                    var res = placement.split("-");
+                    res = res[1].split(new RegExp('[' + separators.join('') + ']', 'g'));
+                    //buildObj.topsectionrow = res[0]; //added shankar for comtopsection placement
+                }
+            }
+
+
+            try {
+                if (dor.hardware.hinge != '') {
+                    buildObj.hardwarehinge = dor.hardware.hinge.visimage;
+                    buildObj.hingeplacement = dor.hardware.hinge.placement;
+                    if (dor.hardware.hinge.count) {
+                        buildObj.hingeplacement = dor.hardware.hinge.placementlist.split(';')[dor.hardware.hinge.count - 1];
+                    } else {
+                        buildObj.hingeplacement = dor.hardware.hinge.placementlist.split(';')[0];
+                    }
+
+                }
+
+            } catch (e) { }
+
+
+            if (dor.hardware.stepplate != '') {
+                buildObj.hardwarestepplate = dor.hardware.stepplate.visimage;
+                buildObj.stepplateplacement = dor.hardware.stepplate.placement;
+            }
+            if (dor.hardware.handle != '') {
+                buildObj.hardwarehandle = dor.hardware.handle.visimage;
+                buildObj.handleplacement = dor.hardware.handle.placement;
+            }
+
+
+            if (buildObj['overlay'] != undefined) {
+                buildObj['overlay'] = '';
+            }
+        }
+        else {
+            if (dor.opener.opener != '') {
+                buildObj.productid = Number(dor.opener.opener.item_id);
+                buildObj.designimage = dor.opener.opener.item_thumbnail;
+            }
+        }
+
+        targ.DoorVisualization('view', viewD)
+        targ.DoorVisualization('update', buildObj)
     }
 
 
