c96be74403d7b5e87761eea7c273bc473cd9e310
Venkata Satya Sai Kiran Mantha <vmantha@evoketechnologies.com>
2017-06-12 01:06:32 +0530

    shopping-cart

--- src/app/door-configuration/door-configuration.component.ts
+++ src/app/door-configuration/door-configuration.component.ts
@@ -101,6 +101,11 @@ export class DoorConfigurationComponent implements OnInit {
 
 
         this.setNavComponent();
+        if (this.utils.resFlowSession.resDoorObj.INSTALLTYPE === 'DIY') {
+            this.utils.resFlowSession.resDetails.totalPrice = this.utils.utilities.itemPriceDY;
+        } else {
+            this.utils.resFlowSession.resDetails.totalPrice = this.utils.utilities.itemPriceInstall;
+        }
     }
     updateQuantity(flow) {
         // this.utils.updateQty will call calculate total amount internally
@@ -121,11 +126,6 @@ export class DoorConfigurationComponent implements OnInit {
     }
 
     nextBtn(path) {
-        if (this.utils.resFlowSession.resDoorObj.INSTALLTYPE === 'DIY') {
-            this.utils.resFlowSession.resDetails.totalPrice = this.utils.utilities.itemPriceDY;
-        } else {
-            this.utils.resFlowSession.resDetails.totalPrice = this.utils.utilities.itemPriceInstall;
-        }
         if (this.utils.utilities.flow === 'gdoNavElems') {
             this.utils.setUtils(5, 1);
             $('.shop-count').text('1');
