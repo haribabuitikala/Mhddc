ce70f9b24eea82fbb9cb475a35c3133f25b8ae8d
Muralikrishna T <muralikrishna8811@gmail.com>
2017-08-14 19:54:49 +0530

    image swap error in local due to canvas wont load

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -712,7 +712,7 @@ export class ConfigComponent implements OnInit, AfterViewInit, AfterViewChecked 
         let count = this.utils.resFlowSession.resDoorObj.QTY;
         if (isIncrement && count < 6) {
             this.utils.resFlowSession.resDoorObj.QTY = count + 1;
-        } else if (count > 1 && count !== 6) {
+        } else if (!isIncrement && count > 1) {
             this.utils.resFlowSession.resDoorObj.QTY = count - 1;
         }
         this.quantity = this.utils.resFlowSession.resDoorObj.QTY;
